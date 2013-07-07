4bae059a457148a2fccd18ee3deaf0e2f900ca3d
JhonAlx <jhadelgadoca@unal.edu.co>
2013-07-07 16:31:40 -0500

    Initial commit

---
+++ Main/src/Soccer.java
@@ -0,0 +1,15 @@
+public class Soccer
+{
+	public int maxPoints(int[] wins, int[] ties)
+	{
+		int max = 0;
+		
+		for(int i = 0; i < wins.length; i++)
+		{
+			max = Math.max(max, (wins[i] * 3) + (ties[i] * 1));
+		}
+		
+		return max;
+	}
+	
+}
